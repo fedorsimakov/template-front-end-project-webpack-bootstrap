2400aec38b87b33bb65549c15ff4c0b489e1b6dd
fedorsimakov <fedor.simakov.a@gmail.com>
2020-04-17 11:26:32 +0500

    first version

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
+import jQuery from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
+
+jQuery(() => {
+  alert ('Privet!');
+  jQuery('body').css('background-color', 'blue');
+});
+
 console.log('Hello, world');
